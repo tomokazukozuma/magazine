b8d0cd176acc2680ef7b130ae82a462691c34800
Tomokazu Kozuma <tomokazu.kozuma@gmail.com>
2019-01-13 18:39:05 +0900

    ユーザ登録のCloud Fucntionsを作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+admin.firestore().settings({
+    timestampsInSnapshots: true
+});
+
+exports.registerUsers = functions.auth.user().onCreate(async user => {
+    const { uid } = user;
+    const db = admin.firestore();
+    const displayName = user.displayName || 'Anonymous';
+    const email = user.email || '';
+    const photoURL = user.photoURL || '/assets/img/default_profile.svg';
+
+    await db.collection('users').doc(uid).set({
+        user_name: displayName,
+        photo_url: photoURL,
+        email,
+        create_on: new Date(),
+    })
+    .catch((err) => {
+        console.log(err); // eslint-disable-line no-console
+    });
+    console.log('Success'); // eslint-disable-line no-console
+    return;
+});
