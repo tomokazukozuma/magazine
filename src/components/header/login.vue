f1936377219b7493388dc1bbeb40923b8abac526
Tomokazu Kozuma <tomokazu.kozuma@gmail.com>
2019-01-28 22:49:39 +0900

    ヘッダーメニューを整理

--- src/components/header/login.vue
+++ src/components/header/login.vue
@@ -1,34 +1,44 @@
 <template>
-    <div v-if="isSignIn" key="login" class="navbar-item">
-        <v-menu offset-y>
-            <v-btn
-                slot="activator"
-                color="primary"
-                dark
-            >
-                {{ user.displayName }}
-            </v-btn>
-            <v-list>
-                <v-list-tile v-for="(item, index) in items" :key="index">
-                    <v-list-tile-title>
-                        <div v-if="item.title=='Sign Out'" key="" class="navbar-item">
-                            <p @click="doLogout">
-                            Sign out
-                            </p>
-                        </div>
-                        <div v-else key="logout">
-                            <router-link v-bind:to="{ name : 'UserMagazine', params : { uid: user.uid }}"><p class="navbar-item">{{ item.title }}</p></router-link>
-                        </div>
-                    </v-list-tile-title>
-                </v-list-tile>
-            </v-list>
-        </v-menu>
-        <!-- <router-link v-bind:to="{ name : 'UserMagazine', params : { uid: user.uid }}"><p class="navbar-item">{{ user.displayName }}</p></router-link> -->
-    </div>
-    <!-- 未ログイン時にはログインボタンを表示 -->
-    <div v-else key="logout">
-        <span><router-link to="/login">Login</router-link></span>
-    </div>
+    <v-toolbar-items>
+        <v-btn flat>
+            投稿
+        </v-btn>
+        <v-btn flat>
+        <div v-if="isSignIn">
+            <!-- <div class="navbar-item">
+                投稿
+            </div> -->
+            <v-menu offset-y>
+                <v-btn
+                    slot="activator"
+                    color="primary"
+                    dark
+                >
+                    {{ user.displayName }}
+                </v-btn>
+                <v-list>
+                    <v-list-tile v-for="(item, index) in items" :key="index">
+                        <v-list-tile-title>
+                            <div v-if="item.title==='Sign Out'">
+                                <p @click="doLogout">
+                                Sign out
+                                </p>
+                            </div>
+                            <div v-else key="logout">
+                                <router-link v-bind:to="{ name : 'UserMagazine', params : { uid: user.uid }}"><p class="navbar-item">{{ item.title }}</p></router-link>
+                            </div>
+                        </v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+            <!-- <router-link v-bind:to="{ name : 'UserMagazine', params : { uid: user.uid }}"><p class="navbar-item">{{ user.displayName }}</p></router-link> -->
+        </div>
+        <!-- 未ログイン時にはログインボタンを表示 -->
+        <div v-else key="logout">
+            <span><router-link to="/login">Login</router-link></span>
+        </div>
+        </v-btn>
+    </v-toolbar-items>
 </template>
 
 <script>
