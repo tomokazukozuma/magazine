0fa1cb5a8d1424c94fd29977a7599bd3edc176ea
Tomokazu Kozuma <tomokazu.kozuma@gmail.com>
2019-02-02 00:44:17 +0900

    取得記事の表示を修正

--- src/components/modal/post_article.vue
+++ src/components/modal/post_article.vue
@@ -4,34 +4,42 @@
         <span slot="activator" color="black" dark>投稿</span>
         <v-card>
             <v-card-title>
-            <span class="headline">取得記事</span>
+                <span class="headline">取得記事</span>
             </v-card-title>
             <v-card-text>
-            <v-container grid-list-md>
-                <v-layout wrap>
-                <v-flex xs12>
-                    <v-text-field label="url*" required v-model="url"></v-text-field>
-                </v-flex>
-                <v-flex xs12>
-                    <span>{{this.title}}</span>
-                </v-flex>
-                <v-flex xs12>
-                    <span>{{this.description}}</span>
-                </v-flex>
-                <v-flex xs12>
-                    <v-img
-                        :src="image"
-                        aspect-ratio="1"
-                    ></v-img>
-                </v-flex>
-                </v-layout>
-            </v-container>
-            <small>*indicates required field</small>
+                <v-container grid-list-md>
+                    <v-layout wrap>
+                    <v-flex xs12>
+                        <v-text-field label="url*" required v-model="url"></v-text-field>
+                        <small>*indicates required field</small>
+                    </v-flex>
+                    <div v-if="image===''">
+                    </div>
+                    <div v-else>
+                    <v-card>
+                        <v-flex xs12>
+                            <v-img
+                                :src="image"
+                                aspect-ratio="1.75"
+                            ></v-img>
+                        </v-flex>
+                        <v-flex xs12>
+                            <v-card-title primary-title>
+                                <div>
+                                    <h3 class="headline mb-0">{{this.title}}</h3>
+                                    <div>{{this.description}}</div>
+                                </div>
+                            </v-card-title>
+                        </v-flex>
+                    </v-card>
+                    </div>
+                    </v-layout>
+                </v-container>
             </v-card-text>
             <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
-            <v-btn color="blue darken-1" flat @click="crawlArticleInfo">Save</v-btn>
+            <v-btn color="blue darken-1" flat @click="crawlArticleInfo">取得</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
